4e9a20ea7fa2867ee83dd568510d02faf0f332d9
Aviv Raz <aviv.raz@cognyte.com>
2024-12-02 21:29:03 +0200

    add comments

--- src/Matala-2/HW2_209266667_206775959_Q3.cpp
+++ src/Matala-2/HW2_209266667_206775959_Q3.cpp
@@ -12,25 +12,31 @@ int main(){
     cout << "Please enter a number" << endl;
     cin >> num;
 
+    // looping until num is less than zero
     while (num > 0) {
+        // remove last digit of num
         numWithOutLastDigitPlace = 1;
         numWithOutLastDigit = 0;
         lastDigitNum = num % 10;
         num = num / 10;
         tempNum = num;
+        // looping until temp num is less than zero
         while (tempNum > 0) {
             lastDigitTempNum = tempNum % 10;
             if (lastDigitTempNum != lastDigitNum) {
+                // create new num without last digit
                 numWithOutLastDigit = numWithOutLastDigit + numWithOutLastDigitPlace * lastDigitTempNum;
                 numWithOutLastDigitPlace *= 10;
             }
             tempNum = tempNum / 10;
         }
+        // create new num without repetition of digits
         newNum = newNum + newNumPlace * lastDigitNum;
         newNumPlace *= 10;
+        // change the value of num variable to hold numbers with digits that equal to last digit
         num = numWithOutLastDigit;
     }
-    cout << "Num is: " << newNum << endl;
+    cout << "newNum: " << newNum << endl;
 
     return 0;
 }

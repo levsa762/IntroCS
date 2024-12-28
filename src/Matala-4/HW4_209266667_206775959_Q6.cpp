894462ddc6ddddc7619e990b64f26366d46e4de7
levsa762 <levsakaju579@gmail.com>
2024-12-28 23:39:18 +0200

    final version

--- src/Matala-4/HW4_209266667_206775959_Q6.cpp
+++ src/Matala-4/HW4_209266667_206775959_Q6.cpp
@@ -1,68 +1,58 @@
 // Name: Lev Sakaju; ID: 209266667
 // Name: Aviv Raz; ID: 206775959
 #include <iostream>
-#include <string.h>
 using namespace std;
 #define SIZE 3
-int mapSequences(char strMat[][50] ,int rows){
+#define COL 100
 
-    int maxc= 0;
-    char tempArray[50]="t";
-    int c1 = 0;
-    int c0 = 0;
-    int indexTemp = 0;
+int mapSequences(char strMat[][COL], int rows) {
+    int maxSequences = 0; 
 
-    for (int i = 0; i < rows; i++)
-    {
-        //tempArray[0] = '\0' ;
-        indexTemp = 0;
-        for (int j = 0; strMat[i][j] != '\0' ; j++)
-        {        
+    for (int i = 0; i < rows; i++) {
+        char tempArray[COL] = {}; // Temporary array to store sequence counts
+        int indexTemp = 0;       // Index for the temporary array
+        int count = 0;           // Counter for the current sequence
+
+        // Check if the row starts with '1', otherwise add '0' to the sequence array
         if (strMat[i][0] != '1') {
-            tempArray[0] = '0';
+            tempArray[indexTemp] = '0';
             indexTemp++;
         }
-        if (strMat[i][j] == '1')
-        {
-            c1++;
-            if (strMat[i][j+1] == '0')
-            {
-                tempArray[indexTemp]=c1;
-                c1=0;
+
+        for (int j = 0; strMat[i][j] != '\0'; j++) {
+            if (j == 0 || strMat[i][j] == strMat[i][j - 1]) {
+                count++; // Increment the count for the current sequence
+            } else {
+                // Add the count of the previous sequence to the array
+                tempArray[indexTemp] = count + '0';
                 indexTemp++;
-            } 
-            
+                count = 1; // Reset the count for the new sequence
+            }
         }
 
-        if (strMat[i][j] == '0')
-        {
-            c0++;
-            if (strMat[i][j+1] == '1')
-            {
-                tempArray[indexTemp]=c1;
-                c0=0;
-                indexTemp++;
-            } 
-            
+        // Add the last sequence count to the array
+        tempArray[indexTemp] = count + '0';
+        indexTemp++;
+        tempArray[indexTemp] = '\0'; 
+
+        // Print the sequence array for the current row
+        cout << tempArray << endl;
+
+        // Update the maximum sequence count if necessary
+        if (indexTemp > maxSequences) {
+            maxSequences = indexTemp;
         }
     }
-    cout << "the number is " << indexTemp;
-    tempArray[indexTemp+1] = '\0';
-    cout << tempArray;
-    cout << endl;
-    if (strlen(tempArray) > maxc)
-    {
-        maxc = strlen(tempArray);
-    }
-   } 
-    return maxc;
+
+    return maxSequences; // Return the maximum sequence count
 }
 
-int main(){
+int main() {
+    char co[SIZE][COL] = {"0000000", "10101", "11000001"};
 
-    //char mo[] = {'p', 'p', '\0', 'l', 'o', '\0'};
-    //cout << strlen(mo);
-    char co [SIZE][50] = {"0000000" ,"10101" ,"11000001"};
+    // Call the function and print the maximum number of sequences
+    int maxSequences = mapSequences(co, SIZE);
+    cout << "Maximum sequences: " << maxSequences << endl;
 
-    mapSequences(co, SIZE);
+    return 0;
 }
